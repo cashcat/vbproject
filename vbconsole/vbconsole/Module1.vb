a9cc7f57baebbb99c094082a062bf9ae7a3dcb67
cashcat <cashcat@gmail.com>
2015-11-08 14:06:17 +0800

    提交console工程

---
+++ vbconsole/vbconsole/Module1.vb
@@ -0,0 +1,18 @@
+﻿
+
+Module Module1
+    ''' <summary>
+    ''' 程序主函数
+    ''' </summary>
+    Sub Main()
+        Dim appDir As String
+        'appDir = Application.StartupPath
+        'appDir = Application.ExecutablePath
+        appDir = System.Environment.CurrentDirectory
+        appDir = My.Computer.FileSystem.CurrentDirectory
+        appDir = AppDomain.CurrentDomain.SetupInformation.ApplicationBase
+        Console.WriteLine(appDir)
+
+    End Sub
+
+End Module
